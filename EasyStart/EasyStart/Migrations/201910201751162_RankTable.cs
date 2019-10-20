ce8a03d02283a6df3f48e17656024fbf392f6a2b
apastuhov <apastuhov@mmtr.ru>
2019-10-20 21:06:50 +0300

    Add rank table migration

---
+++ EasyStart/EasyStart/Migrations/201910201751162_RankTable.cs
@@ -0,0 +1,28 @@
+namespace EasyStart.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class RankTable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.RatingProducts",
+                c => new
+                {
+                    Id = c.Int(nullable: false, identity: true),
+                    ClientId = c.Int(nullable: false),
+                    ProductId = c.Int(nullable: false),
+                    Score = c.Double(nullable: false),
+                })
+                .PrimaryKey(t => t.Id);
+
+        }
+
+        public override void Down()
+        {
+            DropTable("dbo.RatingProducts");
+        }
+    }
+}
