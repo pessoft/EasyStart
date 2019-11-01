289f02ba5f0f2e7b1d7df448dd75c9a52bc4c18a
apastuhov <apastuhov@mmtr.ru>
2019-11-01 15:22:38 +0300

    Intermediate promotion

--- EasyStart/EasyStart/Scripts/promotion/promotion.js
+++ EasyStart/EasyStart/Scripts/promotion/promotion.js
@@ -1,8 +1,65 @@
-﻿$(document).ready(function() {
+﻿$(document).ready(function () {
     $('.promotion-menu li').bind('click', function () { changePromotionActiveMenu(this) })
 })
 
 function changePromotionActiveMenu(e) {
+    const $e = $(e)
     $('.promotion-menu li').removeClass('promotion-menu-active')
-    $(e).addClass('promotion-menu-active')
+    $e.addClass('promotion-menu-active')
+
+    const targetId = $e.attr('target-id')
+    $('.promotion-content').addClass('hide')
+    $(`#${targetId}`).removeClass('hide')
 }
+
+const SotckTypePeriod = {
+    OneOff:1,
+    Infinity: 2,
+    ToDate: 3
+}
+
+var StockManger = {
+    addNewStock: function () {
+        this.cleanStockDialog()
+        this.showStock()
+    },
+    showStock: function (stockId) {
+        const $stockDialog = $('#stockDialog')
+        if (stockId) {
+            //set stock in stock dialog
+        }
+
+        Dialog.showModal($stockDialog)
+        //$stockDialog.find('select').SumoSelect()
+    },
+    saveStockFromDialog: function () {
+    },
+    cleanStockDialog: function () {
+    },
+    nextStockSlide: function (eIdShow, eIdHide) {
+        $(`#${eIdHide}`).hide("slide", { direction: "left" }, 100, () => {
+            $(`#${eIdShow}`).show("slide", { direction: "right" }, 100)
+        });
+    },
+    prevStockSlide: function (eIdShow, eIdHide) {
+        $(`#${eIdHide}`).hide("slide", { direction: "right" }, 100, () => {
+            $(`#${eIdShow}`).show("slide", { direction: "left" }, 100)
+        });
+    },
+    onStockTypePeriodChange: function () {
+        const $e = $('#stock-type-period')
+
+        if ($e.find('option:selected').val() == SotckTypePeriod.OneOff) {
+            const $stockOneTypeSubtype = $('#stock-one-type-subtype')
+            $stockOneTypeSubtype.show('slow')
+            //$stockOneTypeSubtype[0].sumo.reload()
+        } else {
+            const $stockOneTypeSubtype = $('#stock-one-type-subtype')
+            $stockOneTypeSubtype.hide('slow')
+            //$stockOneTypeSubtype[0].sumo.reload()
+        }
+            
+    }
+}
+
+
