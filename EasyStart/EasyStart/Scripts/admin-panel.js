589ee334c3f0a00fc4750a3e9fedf0b8952bf1d7
pess <sharpmsil@gmail.com>
2020-04-19 13:00:07 +0300

    Mark online pay

--- EasyStart/EasyStart/Scripts/admin-panel.js
+++ EasyStart/EasyStart/Scripts/admin-panel.js
@@ -2347,7 +2347,8 @@ class OrderDetailsData {
         this.CashBack = order.CashBack > 0 ?
             `${xFormatPrice(order.CashBack - order.AmountPayDiscountDelivery)} ${prefixRub} (c ${xFormatPrice(order.CashBack)} ${prefixRub})` :
             `${xFormatPrice(order.CashBack)} ${prefixRub}`;
-        this.AmountPayDiscountDelivery = `${xFormatPrice(order.AmountPayDiscountDelivery)} ${prefixRub}`;
+        const markPay = order.BuyType == BuyType.Online ? 'Оплачено ' : ''
+        this.AmountPayDiscountDelivery = `${markPay}${xFormatPrice(order.AmountPayDiscountDelivery)} ${prefixRub}`;
     }
 
     convertAddressInfo(order) {
