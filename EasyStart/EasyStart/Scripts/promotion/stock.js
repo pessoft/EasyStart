e5b54cd64326f6a448b8614ed3dec39159c408cc
pess <sharpmsil@gmail.com>
2020-04-28 15:29:22 +0300

    Fixed stock excluded if null

--- EasyStart/EasyStart/Scripts/promotion/stock.js
+++ EasyStart/EasyStart/Scripts/promotion/stock.js
@@ -209,9 +209,11 @@ var StockManger = {
                 $('#stock-condition-summ-container').show()
                 $('#stock-condition-sum-count').val(data.ConditionOrderSum)
 
-                data.StockExcludedProducts.forEach(p => {
-                    $(`#stock-excluded-products-items`)[0].sumo.selectItem(p.toString())
-                })
+                if (data.StockExcludedProducts) {
+                    data.StockExcludedProducts.forEach(p => {
+                        $(`#stock-excluded-products-items`)[0].sumo.selectItem(p.toString())
+                    })
+                }
                 break;
             case StockConditionTriggerType.ProductsOrder:
                 $('#stock-condition-products-container').show()
