22ca099b1dcacdc3e643492f7470dd4d32424a3c
apastuhov <apastuhov@mmtr.ru>
2021-03-22 10:47:39 +0300

    Added push notification repository

---
+++ EasyStart/EasyStart/Repositories/PushNotificationRepository.cs
@@ -0,0 +1,15 @@
+﻿using EasyStart.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EasyStart.Repositories
+{
+    public class PushNotificationRepository : BaseRepository<OrderModel>
+    {
+        public PushNotificationRepository(DbContext dbContext) : base(dbContext)
+        { }
+    }
+}
