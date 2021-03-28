79420a9b0e1cb1db4075ab607be8fb677ce4e60b
pess <sharpmsil@gmail.com>
2021-03-28 13:06:59 +0300

    Added ApproximateDeliveryTime prop

--- EasyStart/EasyStart/Models/OrderModel.cs
+++ EasyStart/EasyStart/Models/OrderModel.cs
@@ -53,6 +53,7 @@ namespace EasyStart.Models
         public DateTime Date { get; set; }
         public DateTime UpdateDate { get; set; }
         public DateTime? DateDelivery { get; set; }
+        public DateTime? ApproximateDeliveryTime { get; set; }
         public OrderStatus OrderStatus { get; set; } = OrderStatus.Processing;
 
         public IntegrationOrderStatus IntegrationOrderStatus { get; set; } = IntegrationOrderStatus.Unknown;
