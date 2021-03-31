3e60367a03b7b30bd2086dc6f22b0de4b4c35481
pess <sharpmsil@gmail.com>
2021-03-31 21:59:23 +0300

    Added vendor code for AdditionalFilling

--- EasyStart/EasyStart/Models/ProductOption/AdditionalFilling.cs
+++ EasyStart/EasyStart/Models/ProductOption/AdditionalFilling.cs
@@ -10,6 +10,7 @@ namespace EasyStart.Models.ProductOption
         public int Id { get; set; }
         public int BranchId { get; set; }
         public string Name { get; set; }
+        public string VendorCode { get; set; }
         public double Price { get; set; }
         public bool IsDeleted { get; set; }
     }
