070a088f74c188f159c2fd343dfc0529b04dc352
pess <sharpmsil@gmail.com>
2021-06-26 18:23:48 +0300

    Добавлена килограммы и штуки для инфы о продукте

--- EasyStart/EasyStart/Scripts/product-additional-options.js
+++ EasyStart/EasyStart/Scripts/product-additional-options.js
@@ -2,14 +2,18 @@
     Custom: 0,
     Grams: 1,
     Milliliters: 2,
-    Liters: 3
+    Liters: 3,
+    Kilograms: 4,
+    Pieces: 5
 }
 
 const ProductAdditionalInfoTypeShortName = {
     0: '',
     1: 'гр.',
     2: 'мл',
-    3: 'л'
+    3: 'л',
+    4: 'кг',
+    5: 'шт'
 }
 
 function onChangeProductAdditaonalInfoType(e) {
