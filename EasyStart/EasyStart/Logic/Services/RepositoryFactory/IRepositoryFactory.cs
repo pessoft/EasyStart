7c9afece7693dc785b91bc5d41226b49d514bffb
apastuhov <sharpmsil@gmail.com>
2021-07-06 22:57:10 +0300

    Добавлена фабрика репозиториев

---
+++ EasyStart/EasyStart/Logic/Services/RepositoryFactory/IRepositoryFactory.cs
@@ -0,0 +1,17 @@
+﻿using EasyStart.Models;
+using EasyStart.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyStart.Logic.Services.RepositoryFactory
+{
+    public interface IRepositoryFactory
+    {
+        IRepository<T, U> GetRepository<T,U>()
+            where T: class, IBaseEntity<U>, new()
+            where U: IEquatable<U>;
+    }
+}
---
+++ EasyStart/EasyStart/Logic/Services/RepositoryFactory/RepositoryFactory.cs
@@ -0,0 +1,28 @@
+﻿using EasyStart.Models;
+using EasyStart.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyStart.Logic.Services.RepositoryFactory
+{
+    public class RepositoryFactory : IRepositoryFactory
+    {
+        protected DbContext context;
+        
+        public RepositoryFactory(DbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        public IRepository<T, U> GetRepository<T, U>()
+            where T : class, IBaseEntity<U>, new()
+            where U : IEquatable<U>
+        {
+            return new Repository<T, U>(context);
+        }
+    }
+}
